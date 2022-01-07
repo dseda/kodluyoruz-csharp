26185ab720e404952c8703e5a1625e3bc99cd8c5
sed <sedademirgf@gmail.com>
2022-01-07 09:37:59 +0000

    Koleksiyonlar Soru 3 tamamlandı

--- odev-2/Koleksiyonlar-Soru-3/Program.cs
+++ odev-2/Koleksiyonlar-Soru-3/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections;
 namespace Koleksiyonlar_Soru_3
 {
     /*
@@ -11,7 +11,25 @@ namespace Koleksiyonlar_Soru_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string vowels = "aeiıoöuü";
+            ArrayList vowelsInSentence = new ArrayList();
+
+            Console.WriteLine("Bir cümle giriniz");
+            string sentence = Console.ReadLine();
+            
+            foreach(var letter in sentence.ToLower()) {
+                if(vowels.Contains(letter) && !vowelsInSentence.Contains(letter)) {
+                    vowelsInSentence.Add(letter);
+                }
+            }
+            Console.Write("Cümledeki sesli harfler: ");
+
+            foreach (var item in vowelsInSentence)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            
         }
     }
 }
---
+++ odev-2/Koleksiyonlar-Soru-3/readme.md
@@ -0,0 +1,11 @@
+# Koleksiyonlar Soru 3
+
+- [x] Klavyeden girilen cümle içerisindeki sesli harfleri
+      bir dizi içerisinde saklayan,
+
+- [x] Dizinin elemanlarını
+      sıralayan programı yazınız.
+
+## Ekran Çıktısı
+
+![ss](./output3.png)

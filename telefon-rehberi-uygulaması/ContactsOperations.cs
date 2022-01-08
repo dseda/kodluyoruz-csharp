5eb684fbc5c739afb4d39434f5b9069c2847e15a
sed <sedademirgf@gmail.com>
2022-01-08 10:53:17 +0000

    refactor delete contact method

--- telefon-rehberi-uygulaması/ContactsOperations.cs
+++ telefon-rehberi-uygulaması/ContactsOperations.cs
@@ -5,6 +5,18 @@ namespace telefon_rehberi_uygulaması
 {
     class ContactsOperations {
         
+        public void ViewOptions() {
+            Console.WriteLine();
+            Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz :");
+            Console.WriteLine("******************************************");
+            Console.WriteLine("(1) Yeni Numara Kaydetmek");
+            Console.WriteLine("(2) Varolan Numarayı Silmek");
+            Console.WriteLine("(3) Varolan Numarayı Güncelleme");
+            Console.WriteLine("(4) Rehberi Listelemek");
+            Console.WriteLine("(5) Rehberde Arama Yapmak");
+            Console.WriteLine("(6) Çıkış Yapmak");
+            Console.WriteLine();
+        }
         public void AddContact(List<Contact> contactList) {
             Console.WriteLine("Yeni Numara Kaydetmek");
             Console.WriteLine("Lütfen isim giriniz             :");
@@ -15,11 +27,11 @@ namespace telefon_rehberi_uygulaması
             string phone = Console.ReadLine();
             Contact newContact = new Contact(first_name, last_name, phone);
             contactList.Add(newContact);
-            Console.WriteLine("New contact has been added");
+            Console.WriteLine("Kişi rehbere eklendi");
         }
 
         public void DeleteContact(List<Contact> contactList) {
-            Console.WriteLine("Silmek istediğiniz kişinin adını ve soyadını giriniz: ");
+            Console.WriteLine("Silmek istediğiniz kişinin adını ya da soyadını giriniz: ");
             Console.WriteLine("Lütfen isim giriniz    :");
             string first_name = Console.ReadLine();
             Console.WriteLine("Lütfen soyisim giriniz :");
@@ -28,14 +40,22 @@ namespace telefon_rehberi_uygulaması
                 x => x.getFirstName() == first_name ||
                 x.getLastName() == last_name  );
             if(contact != null) {
-                Console.WriteLine("Contact has been deleted");
-                contactList.Remove(contact);
-                Console.WriteLine("Contact has been deleted");
+                Console.WriteLine(contact.getFirstName() + " " + contact.getLastName() 
+                + " isimli kişi rehberden silinmek üzere, onaylıyor musunuz ?(y/n)");
+                string confirmation = Console.ReadLine();
+                if(confirmation == "y" || confirmation == "yes" ){
+                    contactList.Remove(contact);
+                    Console.WriteLine("Kişi rehberden silindi");
+                }
             }
             else{
-                Console.WriteLine("Aradığınız kriterlere uygun veri rehberde bulunamadı. Lütfen bir seçim yapınız.")
+                Console.WriteLine("Aradığınız kriterlere uygun veri rehberde bulunamadı. Lütfen bir seçim yapınız.");
                 Console.WriteLine("* Silmeyi sonlandırmak için : (1)");
                 Console.WriteLine("* Yeniden denemek için      : (2)");
+                int option = Convert.ToInt16(Console.ReadLine());
+                if (option == 2) {
+                    DeleteContact(contactList);
+                } 
             }
         }
         public void ViewContacts(List<Contact> contactList) {
--- telefon-rehberi-uygulaması/Program.cs
+++ telefon-rehberi-uygulaması/Program.cs
@@ -6,36 +6,25 @@ namespace telefon_rehberi_uygulaması
     {
         static void Main(string[] args)
         {   
-            int option=0;
+            int operation=0;
             List<Contact> contactsList = new List<Contact>();
             ContactsOperations ops = new ContactsOperations();
-            do {
-                Console.WriteLine();
-                Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz :");
-                Console.WriteLine("******************************************");
-                Console.WriteLine("(1) Yeni Numara Kaydetmek");
-                Console.WriteLine("(2) Varolan Numarayı Silmek");
-                Console.WriteLine("(3) Varolan Numarayı Güncelleme");
-                Console.WriteLine("(4) Rehberi Listelemek");
-                Console.WriteLine("(5) Rehberde Arama Yapmak");
-                Console.WriteLine("(6) Çıkış Yapmak");
-                Console.WriteLine();
 
-                option = Convert.ToInt16(Console.ReadLine());
-                
-                switch(option) {
+            do {
+                ops.ViewOptions();
+                operation = Convert.ToInt16(Console.ReadLine());
+                switch(operation) {
                     case 1: 
                         ops.AddContact(contactsList);
                         break;
                     case 2: 
                         ops.DeleteContact(contactsList);
                         break;
-                    
                     case 4: 
                         ops.ViewContacts(contactsList);
                         break;
                 }
-        } while (Convert.ToInt16(option) != 6 );
+        } while (Convert.ToInt16(operation) != 6 );
        
 
         }
